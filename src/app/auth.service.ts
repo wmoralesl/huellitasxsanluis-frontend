38f2285fe510464a74db18c5a0fdadce331be45b
WILSON ALBERTO MORALES LAZARO <wilsonm57wm@gmail.com>
2025-03-13 23:33:46 -0600

    [Update]: Iniocio del login

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3048/api/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    const body = { email, password };
+    return this.http.post(this.apiUrl, body);
+  }
+}
