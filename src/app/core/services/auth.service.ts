b1d8624233a3742dbba1e1923857401336c25447
Wilson Morales <wilsonm57wm@gmail.com>
2025-03-27 11:03:55 -0600

    [Bug]: Configuracion funcionando aut

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,36 +1,40 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'http://localhost:3048';
-  private http = inject(HttpClient);
-  private router = inject(Router); // <--- Cambiar constructor por inject()
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/api/auth/login`, { email, password }).pipe(
-      tap((response:any) => {
+    const body = { email, password };
+    return this.http.post(`${this.apiUrl}/api/auth/login`, body).pipe(
+      tap((response: any) => {
         if (response.token) {
-          localStorage.setItem('token', response.token);
+          localStorage.setItem('token', response.token); // Guarda el token en el localStorage
         }
       })
     );
   }
+  
 
   logout(): Observable<any> {
     return this.http.post(`${this.apiUrl}/api/auth/logout`, {}).pipe(
       tap(() => {
-        localStorage.removeItem('token');
-        this.router.navigate(['/login']); // <--- No necesita this.router.navigate ahora
+        localStorage.removeItem('token'); // Elimina el token del localStorage
+        this.router.navigate(['/login']); // Redirige al login
       })
     );
   }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem('token'); // Verifica si hay un token
   }
+
+
 }
