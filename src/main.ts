e96cdafdee294ac52688cd7c736438a4b717b88d
Wilson Morales <wilsonm57wm@gmail.com>
2025-03-27 11:29:35 -0600

    [BUG]: Corregido el bug con el providerhttpClient, estaba siendo declarado en main.ts pero debía ser declarado en app.config.ts :: Solucionado

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { appConfig } from './app/core/config/app.config'; // Importa la configuración
 import { AppComponent } from './app/core/components/app.component';
-import { routes } from './app/core/routing/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(), // Proporciona HttpClient globalmente
-    provideRouter(routes)
-  ]
-}).catch(err => console.error('Aca el error',err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => {
+  console.error('Error en el inicio de la aplicación', err);
+});
