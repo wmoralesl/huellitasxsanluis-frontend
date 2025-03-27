a08e1e3154b09218cb9ad07df9ab6f351ff1e1cb
Wilson Morales <wilsonm57wm@gmail.com>
2025-03-27 15:38:15 -0600

    [Update]: Angular detected that HttpClient is not configured to use etch APIs. (Angular 17 - SSR) | problema resuelto

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from '../routing/app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
   ]
 };
