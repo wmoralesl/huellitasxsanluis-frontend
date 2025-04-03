898c13852c1ade86de0a091c7e299898e4aa2b24
Wilson Morales <wilsonm57wm@gmail.com>
2025-04-03 11:42:25 -0600

    [Update]: Se incluyeron las mascotas en pets

--- src/app/pages/pets/pets.component.ts
+++ src/app/pages/pets/pets.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { Animal } from '../../../models/animal.model';
+import { AnimalsService } from '../../core/services/pets/pets.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.css'
 })
 export class PetsComponent {
+  animals: Animal[] = [];
+  isLoading = true;
+  errorMessage = '';
+
+  constructor(private animalsService: AnimalsService) {}
+  ngOnInit(): void {
+    this.loadAnimals();
+  }
+
+  loadAnimals(): void {
+    this.isLoading = true;
+    this.animalsService.getAllAnimals().subscribe({
+      next: (data) => {
+        this.animals = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Error al cargar las mascotas';
+        this.isLoading = false;
+        console.error('Error:', error);
+      }
+    });
+  }
 
 }
