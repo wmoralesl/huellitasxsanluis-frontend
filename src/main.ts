65a2703c2a45ae3382065840d6c0ba3be0bf40c3
Wilson Morales <wilsonm57wm@gmail.com>
2025-04-05 16:25:45 -0600

    [Feature]: Corrigiendo estructura part2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config'; // Importa la configuración
-import { AppComponent } from './app/core/components/app.component';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => {
   console.error('Error en el inicio de la aplicación', err);
